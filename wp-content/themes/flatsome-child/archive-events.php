4ab5e46a731fc5f01bf85d9c0483830e1251fb5e
NhanTran <phamductdh92@gmail.com>
2022-07-22 20:25:37 +0700

    add categoies template window platfom

---
+++ wp-content/themes/flatsome-child/archive-events.php
@@ -0,0 +1,9 @@
+<?php 
+get_header();
+?>
+
+<div class="events-wrapper events-archive">
+    aaaaa
+</div>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/flatsome-child/taxonomy-categories_events.php
@@ -0,0 +1,9 @@
+<?php 
+get_header();
+?>
+
+<div class="events-wrapper events-archive">
+    bbbbb
+</div>
+
+<?php get_footer(); ?>
