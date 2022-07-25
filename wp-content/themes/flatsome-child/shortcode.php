3ef0e88ed918e5b987d0af50768153369222f025
Phạm Đức <apple@Apples-MacBook-Pro.local>
2022-07-25 17:08:30 +0700

    add sort code news

--- wp-content/themes/flatsome-child/shortcode.php
+++ wp-content/themes/flatsome-child/shortcode.php
@@ -5,11 +5,20 @@
 function NewsAndKnowledge($atts)
 {
     ob_start();
-    $args = array(
-        'post_type' => 'post',
-        'post_status' => 'publish',
-        'posts_per_page' => 3,
-    );
+    if($atts['count_posts']){
+        $args = array(
+            'post_type' => 'post',
+            'post_status' => 'publish',
+            'posts_per_page' => $atts['count_posts'],
+        );
+    }else{
+        $args = array(
+            'post_type' => 'post',
+            'post_status' => 'publish',
+            'posts_per_page' => 3,
+        );
+    }
+    
     $the_query = new WP_Query($args);
 ?>
     <div class="wapper-news-nt">
@@ -22,25 +31,6 @@ function NewsAndKnowledge($atts)
             </div>
         </div>
         <div class="row" id="row-70988312">
-            <div id="col-110291612" class="col medium-4 small-12 large-4">
-				<div class="col-inner">
-			        <div class="box has-hover upcoming-events-box  has-hover box-text-bottom">
-                        <div class="box-image">
-                            <div class="image-cover" style="padding-top:300px;">
-                                <img width="1280" height="960" src="http://localhost/Events/wp-content/uploads/2022/06/blog12.jpg" class="attachment- size- lazy-load-active" alt="" loading="lazy">											
-                            </div>
-					    </div>
-                        <div class="box-text text-center">
-			                <div class="box-text-inner">
-                                <h2 class="text-title">
-                                    <a href="#" class="text-link">KHAI TRƯƠNG KHÁNH THÀNH</a>
-                                </h2>
-                                <p class="text-desc">Lễ khai trương, khánh thành là một sự kiện trọng đại, bởi vì đây là dịp để doanh nghiệp giới thiệu cửa hàng, địa điểm kinh doanh, sản phẩm và dịch vụ tới mọi người.</p>
-                            </div>
-		                </div>
-	                </div>
-	            </div>
-	        </div>
         <?php // The Loop
             if ($the_query->have_posts()) :
                 while ($the_query->have_posts()) : $the_query->the_post();
@@ -49,16 +39,21 @@ function NewsAndKnowledge($atts)
 				<div class="col-inner">
 			        <div class="box has-hover upcoming-events-box  has-hover box-text-bottom">
                         <div class="box-image">
-                            <div class="image-cover" style="padding-top:300px;">
-                                <img width="1280" height="960" src="http://localhost/Events/wp-content/uploads/2022/06/blog12.jpg" class="attachment- size- lazy-load-active" alt="" loading="lazy">											
-                            </div>
+                            <a href="<?php the_permalink(); ?>">
+                                <div class="overlay-box"></div>
+                                <div class="image-cover" style="padding-top:300px;">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <img width="1280" height="960" src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" class="attachment- size- lazy-load-active" alt="" loading="lazy">
+                                    </a>
+                                </div>
+                            </a>
 					    </div>
                         <div class="box-text text-center">
 			                <div class="box-text-inner">
                                 <h2 class="text-title">
-                                    <a href="#" class="text-link">KHAI TRƯƠNG KHÁNH THÀNH</a>
+                                    <a href="<?php the_permalink(); ?>" class="text-link"><?php the_title(); ?></a>
                                 </h2>
-                                <p class="text-desc">Lễ khai trương, khánh thành là một sự kiện trọng đại, bởi vì đây là dịp để doanh nghiệp giới thiệu cửa hàng, địa điểm kinh doanh, sản phẩm và dịch vụ tới mọi người.</p>
+                                <p class="text-desc"><?php echo get_the_excerpt(); ?></p>
                             </div>
 		                </div>
 	                </div>
@@ -73,14 +68,14 @@ function NewsAndKnowledge($atts)
         <div class="row btn-row" id="row-703541777">
             <div id="col-685651024" style="padding: 50px 0px 0px 0px;" class="col upcoming-events-bottom hide-for-small small-12 large-12">
 				<div class="col-inner text-center">
-			        <a href="#" target="_self" class="button primary btn-primary" style="padding:0px 30px 0px 30px;">
+			        <a href="<?php echo $atts['link']; ?>" target="_self" class="button primary btn-primary" style="padding:0px 30px 0px 30px;">
                         <span>Xem tất cả</span>
                     </a>
                 </div>
             </div>
             <div id="col-1216622997" class="col upcoming-events-bottom show-for-small small-12 large-12">
 				<div class="col-inner text-center">
-                    <a href="#" target="_self" class="button primary btn-primary">
+                    <a href="<?php echo $atts['link']; ?>" target="_self" class="button primary btn-primary">
                         <span>Xem tất cả</span>
                     </a>
                 </div>
