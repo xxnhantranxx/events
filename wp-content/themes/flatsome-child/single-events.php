04f8eab4eae062157ebd1e3c49d7bb7bb8d35963
NguyenHa <nguyenha230998@gmail.com>
2022-07-25 15:37:13 +0700

    new img

---
+++ wp-content/themes/flatsome-child/single-events.php
@@ -0,0 +1,80 @@
+<?php 
+get_header();
+?>
+
+<div class="events-wrapper events-single">
+<section class="section">
+        <div class="section-content">
+            <div class="row">
+                <div class="col medium-12 small-12 large-12 header-events-single">
+                    <p>
+                        <a href="#">20 YEAR REUNION</a>
+                    </p>
+                    <h1 class="header-title">Class of 2003</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col medium-4 small-12 large-8">
+                    <div class="event-box-text">
+                        <div class="event-item-img">
+                            <a href="#" class="event-item-link">
+                                <img src="https://cga.wooagency.com.au/wp-content/uploads/2022/03/cga-about-us-hero-image-test-1024x576.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="event-item-content">
+                            <div class="event-container">
+                                <p>The CGA is delighted to host the Class of 2000 & 2001 20-Year Reunion at Hotel Brighton (286 Bay St, Brighton) on Friday 6 August 2021 from 6.30pm — 9.00pm. The emphasis will be on a relaxed evening where you can enjoy each other’s company and catch up with those you haven’t seen in a while.</p>
+                                <p>
+                                    <strong>Tickets</strong>
+                                    : $55 per person and include drinks and nibbles
+                                </p>
+                                <p>
+                                    <strong>Dress</strong>
+                                    : Smart casual
+                                </p>
+                                <p>
+                                    <strong>RSVP</strong>
+                                    : 5pm Friday 30 July 2021
+                                </p>
+                                <p>Please feel free to share this invitation with other Class of 2010 & 2011 Grammarians. We hope to see you there.</p>
+                            </div>
+                            <div class="event-bottom">
+                                <div class="event-navigation">
+                                    <div class="event-navigation-prev">
+                                        <a href="#" class="event-navigation-link">Back to upcoming events</a>
+                                    </div>
+                                    <div class="event-navigation-next">
+                                        <a href="#" class="event-navigation-link">Next event</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col medium-8 small-12 large-4">
+                    <div class="event-item-text">
+                        <div class="event-list">
+                            <div class="event-list-item">
+                                <i class="fa-regular fa-calendar-days event-icon"></i>
+                                <p class="event-content">23.07.2020 — 6:30pm – 9:00pm</p>
+                            </div>
+                            <div class="event-list-item">
+                                <i class="fa-solid fa-ticket event-icon"></i>
+                                <p class="event-content">$55.00 per person</p>
+                            </div>
+                            <div class="event-list-item">
+                                <i class="fa-solid fa-location-dot event-icon"></i>
+                                <p class="event-content">The Deck, 212 Bay St Brighton, VIC 3186 Australia</p>
+                            </div>
+                        </div>
+                        <div class="event-btn">
+                            <a href="#" class="event-btn btn-buy-tickets">Buy tickets</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php get_footer(); ?>
--- wp-content/themes/flatsome-child/taxonomy-categories_events.php
+++ wp-content/themes/flatsome-child/taxonomy-categories_events.php
@@ -3,7 +3,86 @@ get_header();
 ?>
 
 <div class="events-wrapper events-archive">
-    bbbbb
+    <section class="section">
+        <div class="section-content">
+            <div class="row">
+                <div class="col medium-12 small-12 large-12 header-events-archive">
+                    <p>Upcoming Events</p>
+                    <h1 class="header-title">Reunite with your classmates</h1>
+                </div>
+            </div>
+            <div class="row event-item-loop">
+                <div class="col medium-6 small-12 large-6">
+                    <div class="event-item-img">
+                        <a href="#" class="event-item-link">
+                            <img src="https://cga.wooagency.com.au/wp-content/uploads/2022/03/cga-about-us-hero-image-test-1024x576.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col medium-6 small-12 large-6">
+                    <div class="event-item-text">
+                        <p class="event-terms">20 YEAR REUNION</p>
+                        <h2 class="event-title">
+                            <a href="#">Class of 2003</a>
+                        </h2>
+                        <div class="event-list">
+                            <div class="event-list-item">
+                                <i class="fa-regular fa-calendar-days event-icon"></i>
+                                <p class="event-content">23.07.2020 — 6:30pm – 9:00pm</p>
+                            </div>
+                            <div class="event-list-item">
+                                <i class="fa-solid fa-ticket event-icon"></i>
+                                <p class="event-content">$55.00 per person</p>
+                            </div>
+                            <div class="event-list-item">
+                                <i class="fa-solid fa-location-dot event-icon"></i>
+                                <p class="event-content">The Deck, 212 Bay St Brighton, VIC 3186 Australia</p>
+                            </div>
+                        </div>
+                        <div class="event-btns">
+                            <a href="#" class="event-btn btn-buy-tickets">Buy tickets</a>
+                            <a href="#" class="event-btn btn-learn-more">Find out more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row event-item-loop">
+                <div class="col medium-6 small-12 large-6">
+                    <div class="event-item-text">
+                        <p class="event-terms">20 YEAR REUNION</p>
+                        <h2 class="event-title">
+                            <a href="#">Class of 2003</a>
+                        </h2>
+                        <div class="event-list">
+                            <div class="event-list-item">
+                                <i class="fa-regular fa-calendar-days event-icon"></i>
+                                <p class="event-content">23.07.2020 — 6:30pm – 9:00pm</p>
+                            </div>
+                            <div class="event-list-item">
+                                <i class="fa-solid fa-ticket event-icon"></i>
+                                <p class="event-content">$55.00 per person</p>
+                            </div>
+                            <div class="event-list-item">
+                                <i class="fa-solid fa-location-dot event-icon"></i>
+                                <p class="event-content">The Deck, 212 Bay St Brighton, VIC 3186 Australia</p>
+                            </div>
+                        </div>
+                        <div class="event-btns">
+                            <a href="#" class="event-btn btn-buy-tickets">Buy tickets</a>
+                            <a href="#" class="event-btn btn-learn-more">Find out more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col medium-6 small-12 large-6">
+                    <div class="event-item-img">
+                        <a href="#" class="event-item-link">
+                            <img src="https://cga.wooagency.com.au/wp-content/uploads/2022/03/cga-about-us-hero-image-test-1024x576.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 
 <?php get_footer(); ?>
